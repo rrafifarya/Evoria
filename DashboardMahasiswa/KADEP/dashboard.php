782cab6e98a59465d03d1fb5258f70dd1e09ce2d
irsyadrusendra746 <irsyadrusendra746@gmail.com>
2026-06-24 16:06:58 +0700

    Nambah spasi

--- DashboardMahasiswa/KADEP/dashboard.php
+++ DashboardMahasiswa/KADEP/dashboard.php
@@ -22,7 +22,7 @@ $periode = $dataJabatan['periode'] ?? '';
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    <title>EVORIA | Dashboard Kadep</title>
+    <title>EVORIA | Dashboard Kadep </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
